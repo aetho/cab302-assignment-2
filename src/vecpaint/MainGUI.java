69d83163ab0182f3b5e96027a35d814dc6845cb0
Phuoc Nguyen <10571335+aetho@users.noreply.github.com>
2019-05-17 00:47:25 +1000

    Basic interface implemented. JUnit 5 imported.

--- src/vecpaint/MainGUI.java
+++ src/vecpaint/MainGUI.java
@@ -2,6 +2,8 @@ package vecpaint;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainGUI extends JFrame implements Runnable {
     /**
@@ -11,7 +13,8 @@ public class MainGUI extends JFrame implements Runnable {
         // Basic initialization of frame
         setTitle("vecPaint - A graphical VEC file editor");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(960,540);
+        setSize(800,740);
+        setMinimumSize(new Dimension(800,740));
         setLayout(new BorderLayout());
 
         // Set look and feel to operating system's look and feel.
@@ -21,8 +24,86 @@ public class MainGUI extends JFrame implements Runnable {
             // [Todo] Display dialog with exception message.
         }
 
+        addMenu();
+        addToolBar();
+        addContentPanel();
+
         // Show frame
         setVisible(true);
+    }
+
+    /**
+     * Adds the toolbar main GUI.
+     */
+    public void addToolBar(){
+        // [Todo] Attach event handlers to buttons
+        // [Todo] Add colour palette
+        // [Todo] Add colour picker
+        // [Todo] Add colour preview
+        JPanel tPanel = new JPanel(); // toolbar container panel
+        tPanel.setPreferredSize(new Dimension(124,-1));
+        tPanel.setBackground(Utility.GREY900);
+
+        // Adding tool buttons
+        String btnToolsText[] = {"Plot", "Line", "Rectangle", "Ellipse", "Polygon"};
+        Map<String, JButton> btnTools = new HashMap<>();
+        for(String text : btnToolsText){
+            JButton btn = new JButton(text);
+            btn.setPreferredSize(new Dimension(96, 24));
+            btn.setBackground(Utility.GREY900);
+
+            tPanel.add(btn);
+            btnTools.put(text, btn);
+        }
+
+        JPanel colorPanel = new JPanel(); // Colour palette container
+        colorPanel.setLayout(new GridLayout(4,4));
+        colorPanel.setPreferredSize(new Dimension(96,96));
+
+        tPanel.add(colorPanel);
+        add(tPanel, BorderLayout.WEST);
+    }
+
+    /**
+     * Adds the content panel to the main frame.
+     */
+    public void addContentPanel(){
+        JTabbedPane tabs = new JTabbedPane();
+        // Remove Tabbed pane insets
+        UIManager.getInsets("TabbedPane.contentBorderInsets").set(-1,-1,-1,-1);
+
+        JPanel cPanel = new JPanel(); // canvas container panel
+        cPanel.setLayout(new GridBagLayout()); // set layout to Grid bag to centre components inside
+
+        JPanel canvas = new JPanel(); // canvas
+        canvas.setPreferredSize(new Dimension(640, 640));
+
+        cPanel.add(canvas);
+        cPanel.setBackground(Utility.GREY800);
+
+        tabs.add("Untitled.vec",cPanel);
+        add(tabs);
+    }
+
+    /**
+     * Adds the menu to main frame.
+     */
+    public void addMenu(){
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+
+        JMenuItem openFile = new JMenuItem("Open");
+        JMenuItem saveFile = new JMenuItem("Save");
+        JMenuItem closeFile = new JMenuItem("Close");
+        JMenuItem closeAllFiles = new JMenuItem("Close all");
+
+        fileMenu.add(openFile);
+        fileMenu.add(saveFile);
+        fileMenu.add(closeFile);
+        fileMenu.add(closeAllFiles);
+
+        menuBar.add(fileMenu);
+        setJMenuBar(menuBar);
     }
 
     public static void main(String[] args){
---
+++ src/vecpaint/Tests.java
@@ -0,0 +1,6 @@
+package vecpaint;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+public class Tests {
+}
