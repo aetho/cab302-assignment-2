905686cc66fd6210170713f6f1a26336b8029995
Phuoc Nguyen <10571335+aetho@users.noreply.github.com>
2019-05-17 18:56:16 +1000

    Finished up the design of the GUI. Need to add event handling.

--- src/vecpaint/MainGUI.java
+++ src/vecpaint/MainGUI.java
@@ -6,6 +6,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainGUI extends JFrame implements Runnable {
+
+    private JPanel tPanel = new JPanel();                       // toolbar container panel
+    private Map<String, JButton> btnTools = new HashMap<>();    // Tool buttons (plot, line, etc...)
+
+    private JPanel colorPanel = new JPanel();                   // Colour palette container
+    private Map<Color, JButton> btnColors = new HashMap<>();    // Colour palette buttons
+
+    private JTabbedPane tabs = new JTabbedPane();   // Opened files tabbed pane
+    private JPanel cPanel = new JPanel();           // canvas container panel
+    private JPanel canvas = new JPanel();           // canvas panel
+
+    private JPanel pickPanel = new JPanel();    // Colour picker container panel
+    private JButton btnPickPen, btnPickFill;    // Button for picking pen/fill colour
+    private JLabel lblPen, lblFill;             // Labels for Pen/Fill
+
+
+
     /**
      * Creates the main GUI of the program
      */
@@ -17,16 +34,11 @@ public class MainGUI extends JFrame implements Runnable {
         setMinimumSize(new Dimension(800,740));
         setLayout(new BorderLayout());
 
-        // Set look and feel to operating system's look and feel.
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        }catch (Exception e){
-            // [Todo] Display dialog with exception message.
-        }
-
         addMenu();
         addToolBar();
+        addColorPalette();
         addContentPanel();
+        addColorPicker();
 
         // Show frame
         setVisible(true);
@@ -37,50 +49,112 @@ public class MainGUI extends JFrame implements Runnable {
      */
     public void addToolBar(){
         // [Todo] Attach event handlers to buttons
-        // [Todo] Add colour palette
-        // [Todo] Add colour picker
-        // [Todo] Add colour preview
-        JPanel tPanel = new JPanel(); // toolbar container panel
         tPanel.setPreferredSize(new Dimension(124,-1));
         tPanel.setBackground(Utility.GREY900);
 
         // Adding tool buttons
         String btnToolsText[] = {"Plot", "Line", "Rectangle", "Ellipse", "Polygon"};
-        Map<String, JButton> btnTools = new HashMap<>();
         for(String text : btnToolsText){
             JButton btn = new JButton(text);
             btn.setPreferredSize(new Dimension(96, 24));
-            btn.setBackground(Utility.GREY900);
+            btn.setBorderPainted(false);
+            btn.setBackground(Utility.GREY600);
+            btn.setForeground(Color.WHITE);
 
             tPanel.add(btn);
             btnTools.put(text, btn);
         }
 
-        JPanel colorPanel = new JPanel(); // Colour palette container
+        add(tPanel, BorderLayout.WEST);
+    }
+
+    /**
+     * Adds colour palette to toolbar
+     */
+    public void addColorPalette(){
+        // [Todo] Attach event handlers to buttons
         colorPanel.setLayout(new GridLayout(4,4));
         colorPanel.setPreferredSize(new Dimension(96,96));
 
+        Color colors[] = {
+                Utility.RED,
+                Utility.PINK,
+                Utility.PURPLE,
+                Utility.DEEPPURPLE,
+                Utility.INDIGO,
+                Utility.BLUE,
+                Utility.LIGHTBLUE,
+                Utility.CYAN,
+                Utility.TEAL,
+                Utility.GREEN,
+                Utility.LIGHTGREEN,
+                Utility.LIME,
+                Utility.YELLOW,
+                Utility.AMBER,
+                Utility.ORANGE,
+                Utility.DEEPORANGE
+        };
+
+        // Creating and adding colour buttons to colorPanel
+        for(Color c : colors){
+            JButton btn = new JButton();
+            btn.setPreferredSize(new Dimension(24, 24));
+            btn.setBorderPainted(false);
+            btn.setBackground(c);
+
+            colorPanel.add(btn);
+            btnColors.put(c, btn);
+        }
+
+        // Add colour panel to toolbar
         tPanel.add(colorPanel);
-        add(tPanel, BorderLayout.WEST);
+    }
+
+    /**
+     * Adds colour picker to toolbar
+     */
+    public void addColorPicker(){
+        // [Todo] Attach event handlers to buttons
+        pickPanel.setLayout(new GridLayout(2,2, 4, 0));
+        pickPanel.setPreferredSize(new Dimension(96,48));
+        pickPanel.setBackground(Utility.GREY900);
+
+        lblPen = new JLabel("Pen");
+        lblPen.setForeground(Color.WHITE);
+        lblFill = new JLabel("Fill");
+        lblFill.setForeground(Color.WHITE);
+
+        pickPanel.add(lblPen);
+        pickPanel.add(lblFill);
+
+        btnPickPen = new JButton();
+        btnPickFill = new JButton();
+
+        btnPickPen.setBackground(Color.BLACK);
+        btnPickPen.setBorderPainted(false);
+        btnPickFill.setBackground(Color.WHITE);
+        btnPickFill.setBorderPainted(false);
+
+        pickPanel.add(btnPickPen);
+        pickPanel.add(btnPickFill);
+
+        tPanel.add(pickPanel);
     }
 
     /**
      * Adds the content panel to the main frame.
      */
     public void addContentPanel(){
-        JTabbedPane tabs = new JTabbedPane();
         // Remove Tabbed pane insets
         UIManager.getInsets("TabbedPane.contentBorderInsets").set(-1,-1,-1,-1);
 
-        JPanel cPanel = new JPanel(); // canvas container panel
-        cPanel.setLayout(new GridBagLayout()); // set layout to Grid bag to centre components inside
+        // set layout to Grid bag to center components inside
+        cPanel.setLayout(new GridBagLayout());
+        cPanel.setBackground(Utility.GREY800);
 
-        JPanel canvas = new JPanel(); // canvas
         canvas.setPreferredSize(new Dimension(640, 640));
 
         cPanel.add(canvas);
-        cPanel.setBackground(Utility.GREY800);
-
         tabs.add("Untitled.vec",cPanel);
         add(tabs);
     }
---
+++ src/vecpaint/Utility.java
@@ -0,0 +1,31 @@
+package vecpaint;
+
+import java.awt.*;
+
+/**
+ * A Helper class to keep track of common variables used
+ */
+public class Utility {
+    public static final Color GREY600 = Color.decode("#757575");
+    public static final Color GREY700 = Color.decode("#616161");
+    public static final Color GREY800 = Color.decode("#424242");
+    public static final Color GREY900 = Color.decode("#212121");
+
+    public static final Color RED           = Color.decode("#EF5350");
+    public static final Color PINK          = Color.decode("#EC407A");
+    public static final Color PURPLE        = Color.decode("#AB47BC");
+    public static final Color DEEPPURPLE    = Color.decode("#7E57C2");
+    public static final Color INDIGO        = Color.decode("#5C6BC0");
+    public static final Color BLUE          = Color.decode("#42A5F5");
+    public static final Color LIGHTBLUE     = Color.decode("#29B6F6");
+    public static final Color CYAN          = Color.decode("#26C6DA");
+    public static final Color TEAL          = Color.decode("#26A69A");
+    public static final Color GREEN         = Color.decode("#66BB6A");
+    public static final Color LIGHTGREEN    = Color.decode("#9CCC65");
+    public static final Color LIME          = Color.decode("#D4E157");
+    public static final Color YELLOW        = Color.decode("#FFEE58");
+    public static final Color AMBER         = Color.decode("#FFCA28");
+    public static final Color ORANGE        = Color.decode("#FFA726");
+    public static final Color DEEPORANGE    = Color.decode("#FF7043");
+
+}
